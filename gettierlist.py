633e31e3d6d7ed350d2f38344e163219289da5c8
BIGGPIU <114438660+BIGGPIU@users.noreply.github.com>
2024-04-25 13:23:59 -0400

    there ios a bomb

--- gettierlist.py
+++ gettierlist.py
@@ -1,2 +1,14 @@
-def tierlist(level):
+import requests
+from bs4 import BeautifulSoup 
+from time import sleep
+
+def tierlist(level,VER):
+    print ("access")
+    response = requests.get(f"https://piuscores.arroweclip.se/TierLists?Difficulty=18&ChartType=Singles")
+    soup = BeautifulSoup(response.content, 'html.parser') 
+    print(soup.prettify()) 
     return
+
+if __name__ == "__main__":
+
+    tierlist(20,1)
