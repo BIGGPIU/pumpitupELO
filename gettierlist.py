72fb5e3a92b9a670a8d50776af75c0c75e60961d
BIGGPIU <114438660+BIGGPIU@users.noreply.github.com>
2024-04-26 09:39:56 -0400

    gettierlies now prints the html source. Use this to get information about the tier list

--- gettierlist.py
+++ gettierlist.py
@@ -2,6 +2,9 @@ import requests
 from bs4 import BeautifulSoup 
 from time import sleep
 from selenium import webdriver
+from selenium.webdriver.common.by import By
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
 
 #def tierlist(level,VER):
 #    print ("access")
@@ -15,4 +18,15 @@ from selenium import webdriver
 #    tierlist(20,1)
 
 driver = webdriver.Firefox()
-driver.get("https://google.co.in") #this is a test I am just committing this so I can work on it between computers.
+
+driver.implicitly_wait(10)
+
+driver.get("https://piuscores.arroweclip.se/TierLists?Difficulty=18&ChartType=Single") #this is a test I am just committing this so I can work on it between computers.
+element = WebDriverWait(driver,10).until(
+    EC.presence_of_element_located((By.CLASS_NAME,"mud-main-content"))
+)
+if element.text != (""):
+    htmlsource = driver.page_source
+    print (htmlsource)
+
+print (element.text)
