fcf8dbf5740d22e6c315433ea5c51ca3b76e7a60
BIGGPIU <114438660+BIGGPIU@users.noreply.github.com>
2024-04-25 21:36:14 -0400

    nothing

--- gettierlist.py
+++ gettierlist.py
@@ -1,14 +1,18 @@
 import requests
 from bs4 import BeautifulSoup 
 from time import sleep
+from selenium import webdriver
 
-def tierlist(level,VER):
-    print ("access")
-    response = requests.get(f"https://piuscores.arroweclip.se/TierLists?Difficulty=18&ChartType=Singles")
-    soup = BeautifulSoup(response.content, 'html.parser') 
-    print(soup.prettify()) 
-    return
+#def tierlist(level,VER):
+#    print ("access")
+#    response = requests.get(f"https://piuscores.arroweclip.se/TierLists?Difficulty=18&ChartType=Singles")
+#    soup = BeautifulSoup(response.content, 'html.parser') 
+#    print(soup.prettify()) 
+#    return
+#
+#if __name__ == "__main__":
+#
+#    tierlist(20,1)
 
-if __name__ == "__main__":
-
-    tierlist(20,1)
+driver = webdriver.Firefox()
+driver.get("https://google.co.in") #this is a test I am just committing this so I can work on it between computers.
