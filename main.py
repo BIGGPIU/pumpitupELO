d817c005c8717a567bc4b3500062518385c90c12
BIGGPIU <114438660+BIGGPIU@users.noreply.github.com>
2024-05-08 13:07:33 -0400

    attempt (this does not work)

--- main.py
+++ main.py
@@ -14,6 +14,14 @@ modes = ["Single","Double"]
 KEY = "710a11f3-2ec6-48eb-9d51-28116672d435"
 plates = ["Ultimate Game","Perfect game","Fair Game"]#I'm bored I'll do the rest later.
 checkdiff = 15
+OL = [1,7,13,19,25,31,37,43,49,55,61,67,73,79] #len 14 (13)
+VH = [element + 1 for element in OL]
+H = [element + 2 for element in OL]
+M = [element + 3 for element in OL]
+E = [element + 4 for element in OL]
+VE = [element + 5 for element in OL]
+NR = [element + 6 for element in OL]
+
 try:
     f = open("temp.txt","x")
     f.close
@@ -33,27 +41,61 @@ def main():
     hold = readtext()
     hold = hold.replace(".jpg",".png")
     colle = hold.split(".png")
-    f = open(f"{checkdiff}.txt","r")
-    i = 0 
-    a = f.readline()
-    b = f.readline()
-    c = f.readline()
-    d = f.readline()
-    e = f.readline()
-    while i != len(colle):
-        if "level: 15," in colle[i]:
-            temp = re.sub(r'^.*?/songs/', '', colle[i])
-            temp = temp.replace ("&quot","")
-            if temp in a:
-                print ("+1")
-            if temp in b:
-                print ("+2")
-            if temp in c:
-                print ("+3")
-            if temp in d:
-                print ("+4")
-            if temp in e:
-                print ("+5")    
+    request = (response.json())
+    i = 00
+    v = int(request["totalResults"])
+    permidentify = []
+    permlevel = []
+    try:
+        while i != v-1:
+            identify = str(request["results"][i]["chart"]["song"]["imagePath"])
+            level = str(request["results"][i]["chart"]["level"])
+            permidentify.append(identify)
+            permlevel.append(level)
+            print (identify)
+            print (level)
+            i+=1
+    except:
+        pass
+    f= open("tierlistsnapshot.txt","r")
+    i = 0
+    while i != len(permidentify):
+         hold2 = f.readline()
+         hold2 = skimjson(hold2)
+         check = hold2[:2]
+         temp = permlevel[i]
+         temp2 = permidentify[i]
+        
+         if check == "":
+             break
+
+
+         if temp in check:
+            if hold2.find(f"{temp2}&quot") != -1:
+                if i in OL:
+                    print(1)
+                    i+=1
+                if i in VH:
+                    print(2)
+                    i+=1
+                if i in H:
+                    print(3)
+                    i+=1
+                if i in M:
+                    print(4)
+                    i+=1
+                if i in E:
+                    print(5)
+                    i+=1
+                if i in VE:
+                    print(6)
+                    i+=1
+                if i in NR:
+                    print(7)
+                    i+=1
+
+             
+    
 
 
 
@@ -64,8 +106,8 @@ def main():
     x=0 #im going to commit this to work on at home but because you're a forgetful idiot what this is going to do is pull from the list in class DIFF. every time you finish a loop this should go up by 6 (or 7-1) every time you're done with a loop
     listofdiff = ["BLANK",]
     sett = 0
-    while x != 27:
-        pass
+    #while x != 27:
+    #    pass
 
 #actually when I think about it I think I should just put all the difficulties in different files. I think it would make it easier to do all ts :sob:
         
